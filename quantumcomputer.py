7babebc746fa21e8f4628943241dc059c171e35e
markf94 <markfingerhuth@protonmail.com>
2017-01-12 02:04:25 +0200

    working on CNOT

--- quantumcomputer.py
+++ quantumcomputer.py
@@ -28,14 +28,142 @@ class State:
                 raise StandardError('Cannot interpret input of State() creator.'\
                                 ' Please enter a list of valid positions.')
             # initialise state
-            self.state = np.array([1./sqrt(len(lis)) if i in lis else 0 \
+            self.state = np.matrix([1./sqrt(len(lis)) if i in lis else 0 \
                                    for i in range(2**num_qubits)])
         # else if the list has as many entries as amplitudes 2**n,
         # interpret the entries as amplitudes
         elif len(lis) == 2**num_qubits:
             #TODO: check if normalised
-            self.state = np.array(lis)
+            self.state = np.matrix(lis)
 
+        else:
+            raise StandardError('Cannot interpret input of State() creator.'\
+                                ' Please enter a list of valid amplitudes or positions.')
+
+    def print_amplitudes(self):
+        np.set_printoptions(precision=3, suppress = True)
+        print self.state
+
+    def print_basisstates(self):
+        np.set_printoptions(precision=3, suppress = True)
+        print self.state
+
+    def print_full(self):
+        np.set_printoptions(precision=3, suppress = True)
+        for i in range(len(self.state)):
+            basis_string = "".join(( "{0:0", str(int(np.log2(len(self.state)))),"b}"))
+            print round(self.state[i],2), "".join(('  |',basis_string.format(i) , '>' ))
+
+    def print_full_pretty(self):
+        np.set_printoptions(precision=3, suppress = True)
+        data = []
+        for i in range(len(self.state)):
+            basis_string = "".join(( "{0:0", str(int(np.log2(len(self.state)))),"b}"))
+            row = [ self.state[i],  "".join(('  |',basis_string.format(i) , '>' )) ]
+            data.append(row)
+        print
+        print tabulate(data, headers=['Amplitude',' Basis state'] )
+        print
+
+    def project_on_blochsphere(self):
+        alpha = self.state[0]
+        beta = self.state[1]
+        bloch = qp.Bloch() # initialise Bloch sphere
+        # Define the x,y and z axes for the Bloch sphere
+        x_basis = (qp.basis(2,0)+(1+0j)*qp.basis(2,1)).unit()
+        y_basis = (qp.basis(2,0)+(0+1j)*qp.basis(2,1)).unit()
+        z_basis = (qp.basis(2,0)+(0+0j)*qp.basis(2,1)).unit()
+        bloch.add_states([x_basis,y_basis,z_basis]) # add the axes to the Bloch sphere
+        bloch.vector_color = ['k','k','k','k','g'] # Bloch vector colours
+        bloch.vector_width = 3 # define Bloch vector width
+        onestate = [[0.,0.,-1.]] # define |1> state vector
+        bloch.add_vectors(onestate) # add |1> state vector
+
+        # Find and eliminate global phase
+        angle_alpha = cmath.phase(alpha)
+        #print "angle_alpha:", angle_alpha
+        angle_beta = cmath.phase(beta)
+        #print "angle_beta:", angle_beta
+
+        if angle_beta < 0:
+        	if angle_alpha < angle_beta:
+        		alpha_new = alpha/cmath.exp(1j*angle_beta)
+        		beta_new = beta/cmath.exp(1j*angle_beta)
+        	else:
+        		alpha_new = alpha/cmath.exp(1j*angle_alpha)
+                	beta_new = beta/cmath.exp(1j*angle_alpha)
+        else:
+        	if angle_alpha > angle_beta:
+                        alpha_new = alpha/cmath.exp(1j*angle_beta)
+                        beta_new = beta/cmath.exp(1j*angle_beta)
+                else:
+                        alpha_new = alpha/cmath.exp(1j*angle_alpha)
+                        beta_new = beta/cmath.exp(1j*angle_alpha)
+        #print "alpha_new:", alpha_new
+        #print "beta_new:", beta_new
+
+        if abs(alpha) == 0 or abs(beta) == 0:
+        	if alpha == 0:
+        		bloch.clear()
+        		down = [0,0,-1]
+        		bloch.add_vectors(down)
+        	else:
+        		bloch.clear()
+        		up = [0,0,1]
+        		bloch.add_vectors(up)
+        else:
+        	# Compute theta and phi from alpha and beta
+        	theta = 2*cmath.acos(alpha_new)
+        	phi = -1j*cmath.log(beta_new/cmath.sin(theta/2))
+        	#print "theta:", theta
+        	#print "phi:", phi
+
+        	# Compute the cartesian coordinates
+        	x = cmath.sin(theta)*cmath.cos(phi)
+        	y = cmath.sin(theta)*cmath.sin(phi)
+        	z = cmath.cos(theta)
+        	#print "x:", x
+        	#print "y:", y
+        	#print "z:", z
+
+        	# Create the new state vector and plot it onto the Bloch sphere
+        	new_vec = [x.real,y.real,z.real]
+        	bloch.add_vectors(new_vec)
+
+        bloch.show()
+
+    zero_state = np.matrix('1; 0')
+    one_state = np.matrix('0; 1')
+
+####
+## States2
+####
+
+class State2:
+# call with State(number_of_qubits, list_of_basisstatepositions_OR_amplitudes)
+
+    # ground state as default instance of a State
+    def __init__(self, qubits, lis = [0]):
+
+        # if the list has less entries than amplitudes 2**n,
+        # interpret the entries as positions
+        if len(lis) < 2**qubits:
+            # check if all entries are valid positions
+            if any(isinstance(item, complex) \
+                   or item > 2**qubits \
+                   or not isinstance(item, int) \
+                   for item in lis) :
+                raise StandardError('Cannot interpret input of State() creator.'\
+                                ' Please enter a list of valid positions.')
+            # initialise state
+            temp = np.matrix([1./sqrt(len(lis)) if i in lis else 0 \
+                                   for i in range(2**qubits)])
+            self.state = temp.transpose()
+        # else if the list has as many entries as amplitudes 2**n,
+        # interpret the entries as amplitudes
+        elif len(lis) == 2**qubits:
+            #TODO: check if normalised
+            self.state = np.matrix(lis).transpose()
         else:
             raise StandardError('Cannot interpret input of State() creator.'\
                                 ' Please enter a list of valid amplitudes or positions.')
@@ -187,36 +315,65 @@ class Gate:
 class ControlledGate:
 
     def __init__(self, unitary, control, target):
-        #if num_qubits == 2:
-            #self.gate = np.kron(State.zero_state*np.transpose(State.zero_state), Gate.eye) + np.kron(State.one_state*np.transpose(State.one_state), unitary)
-        self.gate = np.zeros((2**num_qubits,2**num_qubits))
-        #qubit_list = [1 if control == i else 0 for i in range(2**(num_qubits))]
-        qubit_list = [0,1,0,1]
-        print qubit_list
-        #State(qubit_list).print_full_pretty()
-        print np.transpose(np.matrix(State(qubit_list).state))*np.matrix(State(qubit_list).state)
-        self.gate = np.zeros((2**num_qubits,2**num_qubits))
-        for k in range(num_qubits):
-            if k == control:
-                self.gate += np.kron(np.transpose(np.matrix(State(qubit_list).state))*np.matrix(State(qubit_list).state), unitary)
-            else:
-                self.gate += np.kron(np.transpose(np.matrix(State(qubit_list).state))*np.matrix(State(qubit_list).state), Gate.eye)
+
+        # CASE 1: ADJACENT QUBITS ARE CHOSEN AS CONTROL AND TARGET
+        if (control == target-1) or (target == control-1):
+
+            control_list = [0 if control == i else 1 for i in range(2)]
+            identity_list = [0 if control_list[j] == 1 else 1 for j in range(2)]
+            # initialize empty 4 x 4 matrix for CNOT
+            self.gate = np.zeros((4,4))
+            for k in range(num_qubits):
+                if k == control:
+                    # if control position is reached:
+                    # perform the outer product |1><1| and, thereafter, the tensor product with the unitary that shall be controlled
+                    self.gate += np.kron(State2(1,control_list).state*State2(1,control_list).state.transpose(), unitary)
+                else:
+                    # perform the outer product |0><0| and, thereafter, the tensor product with the identity matrix
+                    self.gate += np.kron(State2(1,identity_list).state*State2(1,identity_list).state.transpose(), Gate.eye)
+            if num_qubits > 2:
+                order_list = [0]*(num_qubits-1)
+                marker = False
+                for i in range(num_qubits-1):
+                    if (control == i) or (target == i) and (marker == False):
+                        order_list[i] = self.gate
+                        marker = True
+                    else:
+                        order_list[i] = Gate.eye
+
+                #print order_list[1]
+                        #order_list = [self.gate; i += 1; if (control == i) or (target == i) else Gate.eye for i in range(num_qubits)]
+                u_new = order_list[0]
+                for k in range(num_qubits-2):
+                    u_new = np.kron(u_new, order_list[k+1])
+                    self.gate = u_new
+                    print self.gate
+        # CASE 2:
+
 #SCRIPT
 
 # DO NOT CHANGE THE GLOBAL DEFINITION OF num_qubits!
 global num_qubits;
 # define the number of qubits for this simulation
-num_qubits = 2
+num_qubits = 3
 
 
-myState1 = State([0])
+myState1 = State2(3,[1,0,0,0,0,0,0,0])
 newGate = Gate(Gate.X, 0)
+myState1.print_full_pretty()
+myState2 = State2.one_state
+print myState2
 #print myState1.state
-myState1.state = newGate.gate*np.matrix(myState1.state).transpose()
+myState1.state = newGate.gate*myState1.state
+newGate = Gate(Gate.X, 1)
+myState1.state = newGate.gate*myState1.state
+
+#print type(myState1.state)
+myState1.print_full_pretty()
 #print np.matrix(myState1.state).transpose()
-print myState1.state
-CX = ControlledGate(Gate.X,1,0)
+#print myState1.state
+CX = ControlledGate(Gate.X,0,1)
 myState1.state = CX.gate*myState1.state
-print CX.gate
+#print CX.gate
 myState1.print_full_pretty()
 #myState1.project_on_blochsphere()
