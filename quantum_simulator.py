db2487f79c16e2f0ea4383cec88e967fe6f2248b
markf94 <markfingerhuth@protonmail.com>
2017-01-20 00:08:20 +0100

    SWAP test example snippet added

--- quantum_simulator.py
+++ quantum_simulator.py
@@ -382,7 +382,7 @@ def create_controlledGate(gate_matrix, qubit_pos, num_amplitudes, num_qubits):
         return cgate
 
 #################### Execution
-
+'''
 quantum_state = create_state(4,[15])
 print_me(quantum_state, 'full')
 
@@ -391,18 +391,40 @@ quantum_state = apply_total_unitary(X, [0,1,2], quantum_state)
 print_me(quantum_state, 'full')
 #project_on_blochsphere(state)
 measure(quantum_state, 4)
+'''
 
-'''
-# SWAP TEST with psi = |0> and phi = |1>
-state = create_state(3,[0])
-# first qubit is used as ancilla
-state = apply_total_unitary(H,[0],state)
-# flip the third qubit to test if swap test works
-state = apply_total_unitary(X,[2],state)
-state = apply_total_unitary(X,[0,1],state)
-state = apply_total_unitary(X,[0,2],state)
-state = apply_total_unitary(H, [0], state)
-measure(state,100)
-# NEED POSSIBILITY TO MEASURE SINGLE QUBITS ONLY
-# >> TO EASE CONDITIONAL MEASUREMENTS
-'''
+    ##### CODE SNIPPET SWAP TEST #####
+
+# initialize initial quantum state |000>
+# 0th qubit is used as control
+# 1st and 2nd qubit are to be swapped
+quantum_state = create_state(3,[0])
+
+# Put the first qubit into state |+> = 0.707*|0> + 0.7071*|1>
+# Inner product is <q1|q2> = 0.7071
+quantum_state = apply_total_unitary(H,[1],quantum_state)
+
+# print the quantum state before SWAP test
+print_me(quantum_state, 'full')
+
+### START: MAIN SWAP TEST ALGORITHM
+
+# Put control qubit into uniform superposition
+quantum_state = apply_total_unitary(H,[0],quantum_state)
+
+# FREDKIN Gate (controlled SWAP)
+# decomposed into three Toffoli gates
+quantum_state = apply_total_unitary(X,[0,1,2],quantum_state)
+quantum_state = apply_total_unitary(X,[0,2,1],quantum_state)
+quantum_state = apply_total_unitary(X,[0,1,2],quantum_state)
+
+# interfere the two states with another Hadamard on the control qubit
+quantum_state = apply_total_unitary(H, [0], quantum_state)
+
+### END: MAIN SWAP TEST ALGORITHM
+
+# print the final quantum state
+print_me(quantum_state, 'full')
+
+# Measure and gather statistics
+measure(quantum_state,10)
