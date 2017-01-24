9f8fa848c4c17253c3c2ca3a2d65a6fbba561458
markf94 <markfingerhuth@protonmail.com>
2017-01-24 14:48:49 +0200

    coffee break build stable

--- execution_script_py3_tabulate.py
+++ execution_script_py3_tabulate.py
@@ -1,6 +1,3 @@
 from quantum_simulator_py3_tabulate import *
 
 # write code to execute here
-quantum_state = create_state(3,[0])
-quantum_state = apply_unitary(X,[2,1,0],quantum_state)
-print_me(quantum_state, 'probabilities')
